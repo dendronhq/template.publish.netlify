e75d45a13d07dd5cfcf1059b3caedc84ab495557
ScriptAutomate <derek@icanteven.io>
2022-01-27 23:31:01 -0600

    Use yarn, node cache, and nextjs cache

--- dendron-publish-site.sh
+++ dendron-publish-site.sh
@@ -2,22 +2,25 @@
 # dendron-publish-site.sh
 # This script should exist in the root of your workspace
 
-# Remove cached or otherwise present directories meant to be replaced
-rm -rf .next
+# Remove docs directory if present
 rm -rf docs
-rm -rf node_modules
+
+# Uncomment if wishing to remove cache
+# Recommended: Leave cache alone to increase speed of deployments
+#rm -rf .next
+#rm -rf node_modules
 
 # Install latest version of Dendron
-npm install @dendronhq/dendron-cli@latest
+# yarn install @dendronhq/dendron-cli@latest
 # OPTIONALLY
-# Install version of Dendron from package-lock.json in workspace root
+# Install version of Dendron from yarn.lock in workspace root
 # To use:
-# Uncomment the next line, and comment out the other `npm install ...` line
-# npm install
+# Uncomment the next line, and comment out the other `yarn install ...` line
+yarn
 
 # Generate static site with nextjs
-npx dendron publish init
-npx dendron publish export
+yarn dendron publish init
+yarn dendron publish export
 
 # Move generated website to docs directory in workspace root
 mv .next/out docs
